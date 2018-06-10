0d41bfc520f7337e8fd6f33f9640148db6880a52
DomRp <leandroleonardo909@gmail.com>
2018-06-10 10:03:02 -0400

    Actualizacion

--- app/src/main/java/com/example/o_o/class2/MainActivity.java
+++ app/src/main/java/com/example/o_o/class2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.o_o.class2;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -9,9 +10,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatButton btnLogin;
     EditText etCorreo;
+    EditText etClave;
+    HashMap<String, person> data;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -21,23 +27,54 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         // La actividad está siendo creado
 
-        etCorreo = findViewById(R.id.etCorreo);
-        AppCompatButton btnEnter = findViewById(R.id.btnEnter);
-        
-        btnEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String correo = "";
+        etCorreo    =   findViewById(R.id.etCorreo);
+        etClave     =   findViewById(R.id.etPass);
+        btnLogin    =   findViewById(R.id.btnEnter);
+
+        createDummyListOfPeople();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String correo = "";
                 if (etCorreo != null) {
-                    correo = etCorreo.getText().toString();
+                    correo = etCorreo.getText().toString().toLowerCase();
                 }
-                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
-                i.putExtra("NAME", correo);
-                
-                startActivity(i);
+                person currentPerson = data.get(correo);
+
+                if (currentPerson != null) {
+                    Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                    i.putExtra("PROFILE_DATA", currentPerson);
+                    startActivity(i);
+                } else {
+                    showErrorDialog();
+                    etCorreo.setText("");
+                    etClave.setText("");
+                }
             }
         });
     }
+        private void showErrorDialog () {
+        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setMessage(R.string.dialog_body_message);
+            dlgAlert.setTitle(R.string.dialog_title_message);
+            dlgAlert.setPositiveButton("OK", null);
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }
+
+        private void createDummyListOfPeople () {
+        person person = new person();
+            person.setName("Leandro Leonardo");
+            person.setEmail("leandroleonardo909@gmail.com");
+            person.setAbout("I'm a Software Developer specialized on Android Development.");
+            person.setProjects(100);
+            person.setStars(300);
+            person.setRepos(150);
+
+            data = new HashMap<>();
+            data.put("leandroleonardo909@gmail.com", person);
+        }
 
     @Override
     protected void onStart() {
--- app/src/main/java/com/example/o_o/class2/ProfileActivity.java
+++ app/src/main/java/com/example/o_o/class2/ProfileActivity.java
@@ -1,13 +1,26 @@
 package com.example.o_o.class2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class ProfileActivity extends AppCompatActivity {
 
+    private final String PROFILE_DATA = "PROFILE_DATA";
+    person currentPerson;
+
     TextView tvName;
+    TextView textViewAbout;
+    TextView textViewProjects;
+    TextView textViewRepos;
+    TextView textViewStars;
+    AppCompatButton btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +28,44 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         // La actividad está siendo creado
+        // Para dar (setear) este nombre en el bar --> setTitle("Profile");
 
         tvName = findViewById(R.id.textViewName);
+        textViewAbout = findViewById(R.id.textViewAbout);
+        textViewProjects = findViewById(R.id.textViewProjects);
+        textViewRepos = findViewById(R.id.textViewRepos);
+        textViewStars = findViewById(R.id.tvValoraciones);
+        btnShare = findViewById(R.id.btnCompartir);
 
-        String name = getIntent().getExtras().getString("NAME");
-        if(tvName != null){
-            tvName.setText(name);
-            setTitle("Hola!! >> " + name);
-        } else {
-            tvName.setText("Leandro Leonardo");
+        if (getIntent() != null && getIntent().getExtras() != null) {
+            currentPerson = getIntent().getParcelableExtra(PROFILE_DATA);
+
+            if (currentPerson != null) {
+                tvName.setText(currentPerson.getName());
+                textViewAbout.setText(currentPerson.getAbout());
+                textViewProjects.setText(String.valueOf(currentPerson.getProjects()));
+                textViewRepos.setText(String.valueOf(currentPerson.getRepos()));
+                textViewStars.setText(String.valueOf(currentPerson.getStars()));
+            }
         }
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareProfileData(currentPerson);
+            }
+
+            private void shareProfileData(person person) {
+                Intent sendIntent = new Intent();
+
+                String textToSend = person.getName() + "\n" + person.getAbout();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, textToSend);
+                sendIntent.setType("text/plain");
+
+                startActivity(sendIntent);
+            }
+        });
     }
 
     @Override
